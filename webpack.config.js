2573e176d27acbe16eff4304179a6e7edd2c35e4
Igor Czerwinski <czerwony@gmail.com>
2019-03-14 14:12:09 +0000

    Add file-loader to webpack configuration
    File-loader can be used to resolve paths to image assets in the CSS
    files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 module.exports = {
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-    }],
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.jpg$/,
+        use: ['file-loader'],
+      }
+    ],
   },
   devServer: {
     contentBase: 'public',
